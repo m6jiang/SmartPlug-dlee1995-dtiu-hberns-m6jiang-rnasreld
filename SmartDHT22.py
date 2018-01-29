5b257ba3ad6352d782184db843fa8f3bad565531
m6jiang <35510084+m6jiang@users.noreply.github.com>
2018-01-29 13:50:43 -0800

    Update SmartDHT22.py

--- SmartDHT22.py
+++ SmartDHT22.py
@@ -1,1 +1,18 @@
+import Adafruit_DHT
 
+def get_temp_celsius(pin):
+    humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
+
+    return temperature_C
+
+
+def get_temp_fahrenheit(pin):
+    humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
+    temperature_F = temperature_C *9/5.0 + 32
+
+    return temperature_F
+
+def get_humidity(pin):
+    humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
+
+    return humidity
