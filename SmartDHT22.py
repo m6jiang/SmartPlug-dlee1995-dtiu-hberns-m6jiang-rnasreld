c282ec3a14147aed3b777d5e40d2e3371812dbbd
dlee1995 <drl010@ucsd.edu>
2018-02-02 23:26:29 -0800

    trying to fix bugs

--- SmartDHT22.py
+++ SmartDHT22.py
@@ -4,18 +4,21 @@ class SmartDHT:
     def __init__(self,pin):
         humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
         
-    def get_temp_celsius(pin):
+    def get_temp_celsius(SmartDHT):
+        pin = 4
         humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin) 
         
         return temperature_C
     
-    def get_temp_fahrenheit(pin):
+    def get_temp_fahrenheit(SmartDHT):
+        pin = 4
         humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
         temperature_F = temperature_C *9/5.0 + 32
         
         return temperature_F
     
-    def get_humidity(pin):
+    def get_humidity(SmartDHT):
+        pin = 4
         humidity, temperature_C = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
         
         return humidity
