7f3ba3039d6f690b3c6c95abd2ee0d1dfee707d3
dlee1995 <drl010@ucsd.edu>
2018-02-02 23:43:42 -0800

    added counter to main

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ mcpPin = 2
 newMCP = SmartMCP3008.SmartMCP()
 newDHT = SmartDHT22.SmartDHT(dhtPin)
 
+i = 1;
+
 while True:
-    print("MCP reading is: ", newMCP.read(mcpPin))
-    print("DHT Celcius reading is: ", newDHT.get_temp_celcius())
-    print("DHT Farenheit reading is: ", newDHT.get_temp_farenheit())
-    print("DHT Humidity reading is: ", newDHT.get_humidity())
+    print("MCP reading is: ", i, newMCP.read(mcpPin))
+    print("DHT Celcius reading is: ", i, newDHT.get_temp_celcius())
+    print("DHT Farenheit reading is: ", i, newDHT.get_temp_farenheit())
+    print("DHT Humidity reading is: ", i, newDHT.get_humidity())
 
 time.sleep(60)
