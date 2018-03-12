356a09212caa269c650cc100b4d4c0b106ebf504
hberns <35284626+hberns@users.noreply.github.com>
2018-03-12 12:13:32 -0700

    Add files via upload

--- Lab/sense.py
+++ Lab/sense.py
@@ -1,48 +1,154 @@
 import os
-from flask import Flask, render_template, g, request, redirect
+import json
+from flask import Flask, render_template, g, request, redirect, jsonify
 from sqlite3 import dbapi2 as sqlite3
 app = Flask(__name__)
 
-#def get_tempcel(temp):
-#       return celsius
-#def get_tempfah(temp):
-#       return fahrenheit
-#def get_humdity(hum):
-#       return humidity
-#def get_audio(sound):
-#       return gate, envelope, audio
+##### APP SETUP #####
+app = Flask(__name__)
 
-db = sqlite3.connect('lab_4.db')
+##### DB SETUP #####
 
-#db.create("get_cel", 1, get_tempcel)
-#db.create("get_fah", 1, get_tempfah)
-#db.create("get_hum", 1, get_humdity)
-#db.create("get_sound", 1, get_audio)
+# Setup the database credentials
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'sensor.db'),
+    DEBUG=True,
+    SECRET_KEY=b'<SOME HEXADECIMAL SECRET KEY>',
+    USERNAME='admin',
+    PASSWORD='<SOME PASSWORD>'
+))
 
-c = db.cursor()
-c.execute('''DROP TABLE IF EXISTS temperature''')
-c.execute('''CREATE TABLE temperature (id INTEGER PRIMARY KEY autoincrement, fahrenheit INTEGER, celsius INTEGER);''')
-#c.execute('''CREATE TABLE humidity (humidity text)''')
-#c.execute('''CREATE TABLE sound (audio text, envelope text, gate text)''')
-c.execute('''INSERT INTO temperature (fahrenheit, celsius) VALUES (72, 22.2), (75, 23.9), (91, 32.8), (87, 30.6);''')
-#c.execute('''INSERT INTO temperature VALUES (75, 23.9)''')
-#c.execute('''INSERT INTO temperature VALUES (91, 32.8)''')
-#c.execute('''INSERT INTO temperature VALUES (87, 30.6)''')
-#c.execute('''INSERT INTO humidity VALUES (get_humidity)''')
-#c.execute('''INSERT INTO audio VALUES (get_audio)''')
-db.commit()
+# Connect to the DB
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+# Wrap the helper function so we only open the DB once
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+# Create the database (we do this via command line!!!)
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('data.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+# Command to create the database via command line
+# You call it from command line: flask initdb
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+# Close the database when the request ends
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 #db.close()
 @app.route('/')
 def dash():
+    db = get_db()
+    cur = db.execute('SELECT * FROM data')
+    dataSQL = cur.fetchall()
+
+    data_as_dict = []
+    for entry in dataSQL:
+        data_as_dict_sing = {
+            'envelope' : entry[0],
+            'humidity' : round(entry[1],1),
+            'light' : entry[2],
+            'tempC' : round(entry[3],1),
+            'tempF' : round(entry[4],1)
+        }
+        data_as_dict.append(data_as_dict_sing)  
+    dataJSON = json.dumps(data_as_dict)
+    with open('static/data.json','w') as f:
+            f.write(dataJSON)
     return render_template('dashboard.html')
 
-@app.route('/temperature', methods=['GET'])
-def Layout():
-    db = sqlite3.connect('lab_4.db')
-    id=request.args.get('id')
-    if id=='Temperature':
-        pr = db.execute('''SELECT fahrenheit, celsius FROM temperature''')
-        data = pr.fetchall()
-        print(data)
-        return render_template('getTable.html', pr2=data)
+@app.route('/temperature')
+def temp():
+    db = get_db()
+    count_id=request.args.get('count')
+    j=int(count_id)
+    cur = db.execute('SELECT temp_c, temp_f FROM data')
+    dataSQL = cur.fetchall()
+    data_as_dict = []
+    i=0
+    for entry in dataSQL:
+        if i >= 60-j:
+            data_as_dict_sing = {
+                'tempC' : round(entry[0],1),
+                'tempF' : round(entry[1],1)
+            }
+            data_as_dict.append(data_as_dict_sing)
+        i += 1
+    return jsonify(data_as_dict)
+@app.route('/light')
+def light():
+    db = get_db()
+    count_id=request.args.get('count')
+    j=int(count_id)
+    cur = db.execute('SELECT light FROM data')
+    dataSQL = cur.fetchall()
+    data_as_dict = []
+    i=0
+    for entry in dataSQL:
+        if i >= 60-j:
+            data_as_dict_sing = {
+                'light' : round(entry[0],1),
+            }
+            data_as_dict.append(data_as_dict_sing)
+        i += 1
+    return jsonify(data_as_dict)
+@app.route('/sound')
+def sound():
+    db = get_db()
+    count_id=request.args.get('count')
+    j=int(count_id)
+    cur = db.execute('SELECT envelope FROM data')
+    dataSQL = cur.fetchall()
+    count=request.args.get('count')
+    data_as_dict = []
+    i=0
+    for entry in dataSQL:
+        if i >= 60-j:
+            data_as_dict_sing = {
+                'sound' : round(entry[0],1),
+            }
+            data_as_dict.append(data_as_dict_sing)
+        i += 1
+    return jsonify(data_as_dict)
+@app.route('/humidity')
+def humid():
+    db = get_db()
+    count_id=request.args.get('count')
+    j=int(count_id)
+    cur = db.execute('SELECT humidity FROM data')
+    dataSQL = cur.fetchall()
+    data_as_dict = []
+    i=0
+    for entry in dataSQL:
+        if i >= 60-j:
+            data_as_dict_sing = {
+                'humidity' : round(entry[0],1),
+            }
+            data_as_dict.append(data_as_dict_sing)
+        i += 1
+    return jsonify(data_as_dict)
+
+
---
+++ Lab/static/getTable.js
@@ -0,0 +1,139 @@
+var mydata;
+function load() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            mydata = JSON.parse(this.responseText);
+           
+        }
+    };
+
+    request.open("GET", "../static/data.json", true);
+    request.send(null);
+    
+    var chartData = [];
+    var xData = [];
+    labelData = "No data selected"
+    makeChart(chartData, labelData, xData);
+}
+
+function clicked(id) {
+   
+    var num = document.getElementById("count").value;
+    if (num < 1 || num > 60){
+        alert("Error: please choose a number between 1 and 60")
+        return;
+    }
+    var chartData = [];
+    var xData = []
+    	
+        if (id == 0) {
+            var table="<tr><th>Minute</th><th>Temperature (C)</th><th>Temperature (F)</th></tr>";
+            for(i = 60-num; i < 60; i++) {
+                table += "<tr><td>" +
+                (i+1) +
+                "</td><td>" +
+                mydata[i].tempC +
+                "</td><td>" +
+                mydata[i].tempF +
+                "</td></tr>";
+                
+                chartData.push(mydata[i].tempC)
+                xData.push(i+1)
+                
+                }
+           labelData="Temperature (C)"
+           makeChart(chartData, labelData, xData);
+        }
+            
+        if (id == 1) {
+            var table="<tr><th>Minute</th><th>Light</th></tr>";
+            for(i = 60-num; i < 60; i++) {
+                table += "<tr><td>" +
+                (i+1) +
+                "</td><td>" +
+                mydata[i].light +
+                "</td>"
+                
+                chartData.push(mydata[i].light)
+                xData.push(i+1)
+                
+                }
+           labelData="Light Level"
+           makeChart(chartData, labelData, xData);
+        }
+        if (id == 2) {
+            var table="<tr><th>Minute</th><th>Sound</th></tr>";
+            for(i = 60-num; i < 60; i++) {
+                table += "<tr><td>" +
+                (i+1) +
+                "</td><td>" +
+                mydata[i].envelope +
+                "</td>"
+                
+                chartData.push(mydata[i].envelope)
+                xData.push(i+1)
+                
+                }
+           labelData="Sound Envelope"
+           makeChart(chartData, labelData, xData);
+        }
+        if (id == 3) {
+            var table="<tr><th>Minute</th><th>Humidity</th></tr>";
+            for(i = 60-num; i < 60; i++) {
+                table += "<tr><td>" +
+                (i+1) +
+                "</td><td>" +
+                mydata[i].humidity +
+                "</td>"
+                
+            chartData.push(mydata[i].humidity)
+            xData.push(i+1)
+                
+                }
+           labelData="Humidity"
+           makeChart(chartData, labelData, xData);
+        }
+    document.getElementById("mytable").innerHTML = table;
+}
+
+function makeChart(chartData,labelData,xData) {
+     var ctx = document.getElementById("myChart").getContext("2d");
+     var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xData,
+            datasets: [{
+            label: labelData,
+            fill: false,
+            data: chartData,
+            borderColor: 'red'
+                 }]
+        },
+        options: {
+            events: [],
+            scales: {
+                xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Time (min)'
+                        }
+                }],
+                yAxes: [{
+                        ticks: {
+                            suggestedMin: 0
+                        },
+                		display: true,
+						scaleLabel: {
+							display: true,
+							labelString: labelData
+						}
+                    
+                
+                }]
+            }
+        }
+     });
+}
+
